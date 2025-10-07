a0576d86120d643a00c23a41b2436a032d555040
RMS-Ronaldo <ronaldo7472_markes@hotmail.com>
2025-10-07 00:55:04 +0000

    feat: tarefas

---
+++ tarefas/tarefa.cs
@@ -0,0 +1,9 @@
+public class Tarefa 
+{
+    public Id int {get; set;}
+    public Nome string {get; set;}
+    public Descricao string {get; set;}
+    public DataCriacao DateTime {get; set;}
+    public Status int {get; set;}
+    public DataExecucao DateTime {get; set;}
+}
